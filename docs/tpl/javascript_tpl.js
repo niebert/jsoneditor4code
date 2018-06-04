5d959a5fad3c7ff884e3ebd8ba77c3d5b7f8c411
Engelbert Niehaus <niehaus@uni-landau.de>
2018-06-04 15:20:40 +0200

    javascript template updated public/private

--- docs/tpl/javascript_tpl.js
+++ docs/tpl/javascript_tpl.js
@@ -109,9 +109,9 @@ function {{data.classname}} () {
     //---Methods of Class "{{data.classname}}()"
     //---------------------------------------------------------------------
 {{#foreach data.methods data}}
-    // {{visibility}} Method: {{data.classname}}.{{name}}()  
+    // {{visibility}} Method: {{data.classname}}.{{name}}()
     // Parameter:
-    //    {{parameterlist parameter "    //#    "}}
+    //    {{parameterlist parameter "    //    "}}
     // Comment:
 {{indent comment "    //    "}}
     // {{{returncomment}}}
@@ -150,6 +150,7 @@ function {{data.classname}} () {
     }
     // ---- Method: {{name}}() Class: {{data.classname}} ------
 {{/foreach}}
+//-------------------------------------------------------------------------
 {{/ifcond}}
 {{#ifcond data.reposinfo.static "==" "yes"}}
 {{#ifcond data.superclassname "==" ""}}
