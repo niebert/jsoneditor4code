606e0b26c4bcda7994dff47f77122d4c801e8e06
Engelbert Niehaus <niehaus@uni-landau.de>
2019-07-06 09:16:41 +0200

    npm header and tail updated

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 /* ---------------------------------------
  Exported Module Variable: JSONEditor4Code
  Package:  jsoneditor4code
- Version:  1.0.9  Date: 2019/04/03 22:42:40
+ Version:  1.0.9  Date: 2019/07/04 19:09:26
  Homepage: https://niebert.github.io/JSONEditor4Code
  Author:   Engelbert Niehaus
  License:  MIT
- Date:     2019/04/03 22:42:40
+ Date:     2019/07/04 19:09:26
  Require Module with:
     const JSONEditor4Code = require('jsoneditor4code');
     var  compileCode = JSONEditor4Code.compile(vTemplate);
