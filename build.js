f4818ad52176e648a40a690699067ea2c345fc5d
Engelbert Niehaus <niehaus@uni-landau.de>
2019-07-11 15:27:47 +0200

    build.js updated with codegen.js

--- build.js
+++ build.js
@@ -111,7 +111,8 @@ function writeConvertCall() {
   codegen.write_convert_json(vLibDist, vLibDist, pkg);
   console.log("Replacing ___PKG___ variables in generated files DONE: "+vLibOut);
   var vLine = "------------------------------------------------";
-  console.log("\n" + vLine + "Build Process '"+pkg.exportvar+"' DONE");
+  console.log("\n" + vLine);
+  console.log("Build Process '"+pkg.exportvar+"' DONE");
   console.log("README:   '"+pkg.build.readme+"' created");
   console.log("CSS:      '"+pkg.build.css+"' created");
   console.log("HTML:     '"+pkg.build.html+"' created");
