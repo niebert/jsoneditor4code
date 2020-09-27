bfebc9b0277a2ccd1fdcd1c0809afad96c3f75a4
Engelbert Niehaus <niehaus@uni-landau.de>
2020-09-27 14:19:51 +0200

    Added feature to package  - localstorage ID is now dependent on URL

--- src/libs/localstorage.js
+++ src/libs/localstorage.js
@@ -1,8 +1,29 @@
+function get_id4localstorage(pPrefix) {
+  var vID = pPrefix || "LS";
+  if (document) {
+    if (document.location) {
+      if (document.location.href) {
+        var vURL = document.location.href;
+        vURL = vURL.replace(/[^A-Za-z0-9]/g,"");
+        vID += vURL;
+      } else {
+        console.error("ERROR in get_id4localstorage() object 'document.location.href' not defined!");
+      }
+    } else {
+      console.error("ERROR in get_id4localstorage() object 'document.location' not defined!");
+    }
+  } else {
+    console.error("ERROR in get_id4localstorage() object 'document' not defined!");
+  }
+  console.log("Localstorage ID:'" + vID + "'");
+  return vID;
+}
+
 function loadJS(pName) {
     var vName = pName || "class.json";
     if (typeof(Storage) != "undefined") {
         // Store
-        if (typeof(localStorage.getItem(vFileBase)) !== undefined) {
+        if (typeof(localStorage.getItem(get_id4localstorage(vFileBase))) !== undefined) {
           console.log("JSON Data '"+vName+"' try loading from Local Storage");
           var vJSONstring = localStorage.getItem(vFileBase);
           if (!vJSONstring) {
@@ -13,21 +34,21 @@ function loadJS(pName) {
             try {
                 this.setEditorData(JSON.parse(vJSONstring));
             } catch(e) {
-                alert(e)
-            };
-          };
+                alert(e);
+            }
+          }
         } else {
           console.log("JSON-Data '"+vName+"' is undefined in Local Storage.\nSave default as JSON");
-          localStorage.setItem(vFileBase, JSON.stringify(editor.getValue()));
-        };
+          localStorage.setItem(get_id4localstorage(vFileBase), JSON.stringify(editor.getValue()));
+        }
         // Load the last edited filename from LocalStorage
         if (typeof(localStorage.getItem("filename")) !== undefined) {
           var vFile = localStorage.getItem("filename");
           $('#load_filename').html(vFile); // this.value.replace(/.*[\/\\]/, '')
-        };
+        }
     }	 else {
         console.log("WARNING: Sorry, your browser does not support Local Storage of JSON Database. Use Firefox ...");
-    };
+    }
 }
 
 
@@ -40,16 +61,16 @@ function saveLS(pName) {
         console.log("JSON-DB '"+vName+"' is defined, JSONDB in  Local Storage");
         if (vData) {
           //console.log("pJSONDB '"+vName+"' is saved to Local Storage");
-          var vJSONstring = JSON.stringify(vData)
+          var vJSONstring = JSON.stringify(vData);
           console.log("saveLS('"+vName+"') JSONstring='"+vJSONstring.substr(0,120)+"...'");
-          localStorage.setItem(vFileBase,vJSONstring);
+          localStorage.setItem(get_id4localstorage(vFileBase),vJSONstring);
           localStorage.setItem("filename",vName);
         } else {
           console.log("vData with JSON is NOT defined");
         }
       } else {
         console.log("JSON Data is undefined");
-      };
+      }
     }	 else {
       console.log("WARNING: Sorry, your browser does not support Local Storage of JSON Database. Use Firefox ...");
     }
