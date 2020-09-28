c068c6e1b87fa4b8586c46567ce97268f8c77c96
Engelbert Niehaus <niehaus@uni-landau.de>
2020-09-28 16:43:12 +0200

    Bugfix of package - build4code updated package.json update libraries

--- dist/jsoneditor4code.js
+++ dist/jsoneditor4code.js
@@ -1,11 +1,11 @@
 /* ---------------------------------------
  Exported Module Variable: JSONEditor4Code
  Package:  jsoneditor4code
- Version:  1.1.20  Date: 2020/09/28 16:39:29
+ Version:  1.1.21  Date: 2020/09/28 16:42:53
  Homepage: https://niebert.github.io/JSONEditor4Code
  Author:   Engelbert Niehaus
  License:  MIT
- Date:     2020/09/28 16:39:29
+ Date:     2020/09/28 16:42:53
  Require Module with:
     const JSONEditor4Code = require('jsoneditor4code');
  JSHint: installation with 'npm install jshint -g'
--- src/readme_tail.md
+++ src/readme_tail.md
@@ -1,10 +1,10 @@
 ## NPM Library Information
 * Exported Module Variable: `JSONEditor4Code`
 * Package:  `jsoneditor4code`
-* Version:  `1.1.20`   (last build 2020/09/28 16:39:29)
+* Version:  `1.1.21`   (last build 2020/09/28 16:42:53)
 * Homepage: `https://niebert.github.io/JSONEditor4Code`
 * License:  MIT
-* Date:     2020/09/28 16:39:29
+* Date:     2020/09/28 16:42:53
 * Require Module with:
 ```javascript
     const vJSONEditor4Code = require('jsoneditor4code');
