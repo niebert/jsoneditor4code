fb428b0e524d2c7f82b63e36e51a5a4a2783d014
Engelbert Niehaus <niehaus@uni-landau.de>
2020-10-07 14:34:47 +0200

    Update Handlebars4Code to Version 1.2.37

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 /* ---------------------------------------
  Exported Module Variable: JSONEditor4Code
  Package:  jsoneditor4code
- Version:  1.1.27  Date: 2020/10/07 14:29:41
+ Version:  1.1.28  Date: 2020/10/07 14:31:41
  Homepage: https://niebert.github.io/JSONEditor4Code
  Author:   Engelbert Niehaus
  License:  MIT
- Date:     2020/10/07 14:29:41
+ Date:     2020/10/07 14:31:41
  Require Module with:
     const JSONEditor4Code = require('jsoneditor4code');
  JSHint: installation with 'npm install jshint -g'
