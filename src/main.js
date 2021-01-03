c6d0dd77b542605b29c961f3edb0cb107c2981ab
Engelbert Niehaus <niehaus@uni-landau.de>
2021-01-03 12:41:35 +0100

    README updated with Demo Link

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 /* ---------------------------------------
  Exported Module Variable: JSONEditor4Code
  Package:  jsoneditor4code
- Version:  1.1.33  Date: 2021/01/03 12:34:16
+ Version:  1.1.34  Date: 2021/01/03 12:40:57
  Homepage: https://niebert.github.io/JSONEditor4Code
  Author:   Engelbert Niehaus
  License:  MIT
- Date:     2021/01/03 12:34:16
+ Date:     2021/01/03 12:40:57
  Require Module with:
     const JSONEditor4Code = require('jsoneditor4code');
  JSHint: installation with 'npm install jshint -g'
--- src/readme_tail.md
+++ src/readme_tail.md
@@ -1,10 +1,10 @@
 ## NPM Library Information
 * Exported Module Variable: `JSONEditor4Code`
 * Package:  `jsoneditor4code`
-* Version:  `1.1.33`   (last build 2021/01/03 12:34:16)
+* Version:  `1.1.34`   (last build 2021/01/03 12:40:57)
 * Homepage: `https://niebert.github.io/JSONEditor4Code`
 * License:  MIT
-* Date:     2021/01/03 12:34:16
+* Date:     2021/01/03 12:40:57
 * Require Module with:
 ```javascript
     const vJSONEditor4Code = require('jsoneditor4code');
